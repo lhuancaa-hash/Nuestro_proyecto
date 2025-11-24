611297a8e7a6b97fc7dc348fe3b7648e7729bb6e
NitaAguirre <167264249+NitaAguirre@users.noreply.github.com>
2025-11-23 21:21:44 -0400

    Se agrego los ulr's de Producto

--- proygrupo/appTienda/urls.py
+++ proygrupo/appTienda/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('movimientos/crear/', views.crear_movimiento, name='crear_movimiento'),
     path('movimientos/modificar/<int:id>/', views.modificar_movimiento, name='modificar_movimiento'),
     path('movimientos/eliminar/<int:id>/', views.eliminar_movimiento, name='eliminar_movimiento'),
+    path('productos/', views.listar_productos, name='listar_productos'),
+    path('productos/crear/', views.crear_producto, name='crear_producto'),
+    path('productos/modificar/<int:id>/', views.modificar_producto, name='modificar_producto'),
+    path('productos/eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
 ]
