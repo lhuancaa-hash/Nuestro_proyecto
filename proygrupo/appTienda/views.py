3ef8d79982cf234da469a6a1318a0246a4fa6cce
NitaAguirre <167264249+NitaAguirre@users.noreply.github.com>
2025-11-23 21:20:25 -0400

    Se agrego el CRUD de Producto.

--- proygrupo/appTienda/views.py
+++ proygrupo/appTienda/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import MovimientoStock
 from .forms import MovimientoStockForm
+from .models import Producto
+from .forms import ProductoForm
 
 def listar_movimientos(request):
     movimientos = MovimientoStock.objects.all().order_by('id_mov')
@@ -33,3 +35,35 @@ def eliminar_movimiento(request, id):
         movimiento.delete()
         return redirect('listar_movimientos')
     return redirect('listar_movimientos')
+
+def listar_productos(request):
+    productos = Producto.objects.all().order_by('id_prod')
+    return render(request, 'listar_productos.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'formulario_producto.html', {'form': form, 'titulo': 'Crear Producto'})
+
+def modificar_producto(request, id):
+    producto = get_object_or_404(Producto, id_prod=id)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'modificar_producto.html', {'form': form, 'producto': producto})
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id_prod=id)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return redirect('listar_productos')
