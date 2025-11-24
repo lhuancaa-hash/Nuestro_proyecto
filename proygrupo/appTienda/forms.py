0a350beb5256f74e12d47b41cb5b49ef8befe763
NitaAguirre <167264249+NitaAguirre@users.noreply.github.com>
2025-11-23 21:17:18 -0400

    Creamos un formulario para Producto.

--- proygrupo/appTienda/forms.py
+++ proygrupo/appTienda/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import MovimientoStock
+from .models import Producto
 
 class MovimientoStockForm(forms.ModelForm):
     cantidad = forms.IntegerField(
@@ -26,4 +27,25 @@ class MovimientoStockForm(forms.ModelForm):
             'cantidad': 'Cantidad',
             'usuario': 'Usuario',
             'motivo': 'Motivo',
+
+        }
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'categoria', 'precio_compra', 'precio_venta', 'unidad_medida', 'id_prov']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.TextInput(attrs={'class': 'form-control'}),
+            'precio_compra': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'precio_venta': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'unidad_medida': forms.TextInput(attrs={'class': 'form-control'}),
+            'id_prov': forms.Select(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'nombre': 'Nombre del Producto',
+            'categoria': 'Categoría',
+            'precio_compra': 'Precio de Compra',
+            'precio_venta': 'Precio de Venta',
+            'unidad_medida': 'Unidad de Medida',
+            'id_prov': 'Proveedor',
         }
